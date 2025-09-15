3a15750cd6edebd520ea2e5ad777a1b173ad195e
Abbas Akhavan <mr.abbas.akhavan@gmail.com>
2025-09-15 22:39:28 +0330

    slider fetch with no store cache

--- components/homepage/Slider.tsx
+++ components/homepage/Slider.tsx
@@ -5,7 +5,8 @@ const Slider = async () => {
         headers: {
             apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwdWVuaXR5am9rZmpobXV0amp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczMjgxNDgsImV4cCI6MjA3MjkwNDE0OH0.e1JlTc9hN3ZORLryHcV1g2K2w26Mqa9Nv5iybHk0KZM',
             Authorization: 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpwdWVuaXR5am9rZmpobXV0amp2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTczMjgxNDgsImV4cCI6MjA3MjkwNDE0OH0.e1JlTc9hN3ZORLryHcV1g2K2w26Mqa9Nv5iybHk0KZM'
-        }
+        },
+        cache: 'no-store'
     });
     const data: SliderItem[] = await res.json();
     console.log(data)
