e1569f0cf96637f9d739169ad682aefc56dbadea
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-09-23 22:46:56 +0330

    start category section

---
+++ components/homepage/CategoriesContainer.tsx
@@ -0,0 +1,118 @@
+import React from 'react'
+interface Category {
+    id: string;
+    title: string;
+    image_url: string;
+    link: string;
+}
+
+const categories: Category[] = [
+    {
+        id: "category1",
+        title: "موبایل",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/09a98a13c782e12a245930b4515d243b17734a33_1740299441.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category2",
+        title: "لپ تاپ",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/7cf1fed6dac6bdfd1b888db6bf8f443ea680244b_1748692252.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category1",
+        title: "موبایل",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/09a98a13c782e12a245930b4515d243b17734a33_1740299441.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category2",
+        title: "لپ تاپ",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/7cf1fed6dac6bdfd1b888db6bf8f443ea680244b_1748692252.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category1",
+        title: "موبایل",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/09a98a13c782e12a245930b4515d243b17734a33_1740299441.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category2",
+        title: "لپ تاپ",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/7cf1fed6dac6bdfd1b888db6bf8f443ea680244b_1748692252.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category1",
+        title: "موبایل",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/09a98a13c782e12a245930b4515d243b17734a33_1740299441.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category2",
+        title: "لپ تاپ",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/7cf1fed6dac6bdfd1b888db6bf8f443ea680244b_1748692252.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category1",
+        title: "موبایل",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/09a98a13c782e12a245930b4515d243b17734a33_1740299441.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category2",
+        title: "لپ تاپ",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/7cf1fed6dac6bdfd1b888db6bf8f443ea680244b_1748692252.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category1",
+        title: "موبایل",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/09a98a13c782e12a245930b4515d243b17734a33_1740299441.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category2",
+        title: "لپ تاپ",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/7cf1fed6dac6bdfd1b888db6bf8f443ea680244b_1748692252.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category1",
+        title: "موبایل",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/09a98a13c782e12a245930b4515d243b17734a33_1740299441.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category2",
+        title: "لپ تاپ",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/7cf1fed6dac6bdfd1b888db6bf8f443ea680244b_1748692252.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category1",
+        title: "موبایل",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/09a98a13c782e12a245930b4515d243b17734a33_1740299441.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+    {
+        id: "category2",
+        title: "لپ تاپ",
+        image_url: "https://dkstatics-public.digikala.com/digikala-mega-menu/7cf1fed6dac6bdfd1b888db6bf8f443ea680244b_1748692252.jpg?x-oss-process=image/resize,m_lfit,h_300,w_300/quality,q_80",
+        link: "/categories/mobile",
+    },
+]
+async function getData() {
+    await new Promise(resolve => setTimeout(resolve, 3000))
+    return categories
+}
+const CategoriesContainer = async () => {
+    const data = await getData();
+    return (
+        <div>CategoriesContainer</div>
+    )
+}
+
+export default CategoriesContainer
