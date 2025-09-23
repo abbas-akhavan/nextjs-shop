cbb1d7df73653cbd6b87613342b894f6d60ab95c
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-09-23 17:21:59 +0330

    add key props for banners

--- components/skeletons/Grid4BannerSkeleton.tsx
+++ components/skeletons/Grid4BannerSkeleton.tsx
@@ -5,7 +5,7 @@ const Grid4BannerSkeleton = () => {
         <div className='grid grid-cols-2 md:grid-cols-4 gap-5 mt-5'>
             {
                 Array.from({ length: 4 }, (_, index) => (
-                    <div className='skeleton-bg rounded-lg aspect-[365/274]'></div>
+                    <div key={index} className='skeleton-bg rounded-lg aspect-[365/274]'></div>
                 ))
             }
         </div>
--- components/skeletons/ServicesSkeleton.tsx
+++ components/skeletons/ServicesSkeleton.tsx
@@ -5,7 +5,7 @@ const ServicesSkeleton = () => {
         <div className='mt-5 flex justify-around gap-6 overflow-auto overflow-y-hidden hide-scrollbar'>
             {
                 Array.from({ length: 13 }, (_, index) => (
-                    <div className='flex flex-col gap-2 items-center w-14 shrink-0'>
+                    <div key={index} className='flex flex-col gap-2 items-center w-14 shrink-0'>
                         <div className='size-[52px] rounded-full skeleton-bg'>
                         </div>
                         <span className='text-sm text-center skeleton-bg w-full h-4 rounded-md'></span>
