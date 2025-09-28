e353b1aefe43f79b2c2fa674f55e93ea12ec6275
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-09-28 17:50:43 +0330

    remove commented codes from Grid4BannerContainer

--- components/homepage/Grid4BannerContainer.tsx
+++ components/homepage/Grid4BannerContainer.tsx
@@ -4,37 +4,6 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react'
 
-// const partnerShips: PartnerShip[] = [
-//     {
-//         id: "partner-ship1",
-//         title: "پارتنر شیپ تاج",
-//         banner: "https://dkstatics-public.digikala.com/digikala-adservice-banners/872e00ff227aff4890c122808cad30494ed26aeb_1757854763.png?x-oss-process=image/quality,q_95",
-//         link: "/landing/taj",
-//     },
-//     {
-//         id: "partner-ship2",
-//         title: "پارتنر شیپ زرین",
-//         banner: "https://dkstatics-public.digikala.com/digikala-adservice-banners/304d40c7d6580cf46db2d40473d15ca905c871a2_1758454014.jpg?x-oss-process=image/quality,q_95",
-//         link: "/landing/zarin",
-//     },
-
-//     {
-//         id: "partner-ship3",
-//         title: "پارتنر شیپ عروس",
-//         banner: "https://dkstatics-public.digikala.com/digikala-adservice-banners/434da6ee067ce7d8094afd839461b1a758542bfb_1758010985.jpg?x-oss-process=image/quality,q_95",
-//         link: "/landing/aroos",
-//     },
-//     {
-//         id: "partner-ship4",
-//         title: "پارتنر شیپ لوکس",
-//         banner: "https://dkstatics-public.digikala.com/digikala-adservice-banners/439b763258dcff90958e9b9bd0069f67b7cdbc6b_1757930148.jpg?x-oss-process=image/quality,q_95",
-//         link: "/landing/lux",
-//     },
-// ];
-// async function getData() {
-//     await new Promise(resolve => setTimeout(resolve, 3000))
-//     return partnerShips
-// }
 const Grid4BannerContainer = async () => {
     const data: PartnerShip[] = await fetchFromSupabase('partner_ships', {
         select: '*',
