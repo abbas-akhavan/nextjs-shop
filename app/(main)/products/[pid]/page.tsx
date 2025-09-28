fe99f0ae3d7a61e95940aa9ba1d8a489995f66b0
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-09-29 01:08:43 +0330

    start produts and single product page

---
+++ app/(main)/products/[pid]/page.tsx
@@ -0,0 +1,18 @@
+import { fetchFromSupabase } from '@/lib/helpers/supabase-ssr'
+import { Product } from '@/types/Product';
+import React from 'react'
+
+const SingleProduct = async ({ params }: { params: { pid: string } }) => {
+    const { pid } = await params;
+    const data: Product[] = await fetchFromSupabase('products', {
+        filters: {
+            'id': `eq.${pid}`
+        }
+    })
+    const product: Product = data[0];
+    return (
+        <div>{product.name}</div>
+    )
+}
+
+export default SingleProduct
---
+++ app/(main)/products/page.tsx
@@ -0,0 +1,20 @@
+import { fetchFromSupabase } from '@/lib/helpers/supabase-ssr'
+import { Product } from '@/types/Product'
+import React from 'react'
+
+const page = async () => {
+    const products: Product[] = await fetchFromSupabase('products', {
+        select: '*'
+    })
+    return (
+        <div>
+            {
+                products.map(product => (
+                    <h2 key={product.id}>{product.name}</h2>
+                ))
+            }
+        </div>
+    )
+}
+
+export default page
