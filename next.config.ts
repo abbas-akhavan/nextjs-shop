4855a0ef7901b7fd74b7de5e7e9579dca885e62e
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-09-29 17:24:46 +0330

    remove global not found

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    globalNotFound: true,
-  },
   images: {
     domains: ["zpuenityjokfjhmutjjv.supabase.co", "dkstatics-public.digikala.com"],
   },
