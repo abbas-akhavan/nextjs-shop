cec12bb3b61cbb8211122159b00db25074b9deaa
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-09-30 16:44:15 +0330

    add userToken option to fetchFromSupabase function params

--- lib/helpers/supabase-ssr.ts
+++ lib/helpers/supabase-ssr.ts
@@ -2,20 +2,20 @@ interface SupabaseQuery {
     select?: string;
     filters?: Record<string, string>;
     next?: NextFetchRequestConfig;
+    userToken?: string;
 }
 export async function fetchFromSupabase(table: string, params?: SupabaseQuery) {
     const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
-    const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY;
+    const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
     const queryParams = new URLSearchParams({
         select: params?.select || '*',
         ...params?.filters
     });
-
     const response = await fetch(`${SUPABASE_URL}/rest/v1/${table}?${queryParams}`, {
         headers: {
             apikey: `${SUPABASE_ANON_KEY}`,
-            Authorization: `Bearer ${SUPABASE_ANON_KEY}`,
+            Authorization: `Bearer ${params?.userToken || SUPABASE_ANON_KEY}`,
             'Content-Type': 'application/json'
         },
         next: params?.next
