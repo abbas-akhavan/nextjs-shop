333064f4e1798b1204dfaea5aca6d8334fb24944
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-03 16:40:01 +0330

    work on cart page

--- app/(main)/cart/page.tsx
+++ app/(main)/cart/page.tsx
@@ -1,4 +1,5 @@
 import CartDetails from '@/components/cart/CartDetails'
+import ReactQueryProvider from '@/providers/Providers'
 import React from 'react'
 
 const page = () => {
--- components/cart/CartDetails.tsx
+++ components/cart/CartDetails.tsx
@@ -4,13 +4,17 @@ import useAppStore from '@/store/useAppStore';
 import { CartItem } from '@/types/AppStateTypes';
 import { supabase } from '@/utils/supabaseClient';
 import { useQuery } from '@tanstack/react-query';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react'
+import CartButton from '../shared/CartButton';
+import Price from '../shared/Price';
+import LoadingComponent from '../LoadingComponent';
 
 const CartDetails = () => {
     const user = useAppStore(state => state.user);
     const router = useRouter();
-    const { data, isLoading, error } = useQuery({
+    const { data: cartItems, isLoading, error } = useQuery({
         queryKey: ['cart'],
         queryFn: async () => {
             const { data: cartId } = await supabase.rpc('get_or_create_active_cart');
@@ -19,7 +23,7 @@ const CartDetails = () => {
                 filters: {
                     'cart_id': `eq.${cartId}`
                 },
-                userToken: user?.token
+                userToken: user?.userInfo?.token
             })
             return cartItems
         },
@@ -27,13 +31,32 @@ const CartDetails = () => {
         refetchInterval: 10000
     })
     useEffect(() => {
-        if (!user) router.push('/auth/login')
+        if (!user.isLoading && !user.userInfo) router.push('/auth/login')
     }, [])
 
+    if (user.isLoading || isLoading) return <LoadingComponent />
+
+    if (error) return <div>{error.message}</div>
     return (
         <div className='container grid grid-cols-[1fr_300px] gap-3'>
             <div>
                 <div>سبد خرید شما</div>
+                <div className='flex flex-col gap-2'>
+                    {
+                        cartItems?.map(({ product, quantity }) => (
+                            <div className='flex flex-col gap-2 border-b border-gray-600 pb-2 last:border-0' key={product.id}>
+                                <div className='flex gap-2'>
+                                    <Image className='rounded-sm' src={product.image_url} alt={product.name} width={90} height={90} />
+                                    <div className='text-sm line-clamp-2 h-10'>{product.name}</div>
+                                </div>
+                                <div className='flex items-center gap-3'>
+                                    <CartButton product={product} />
+                                    <Price value={product.price * quantity} />
+                                </div>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
 
             <aside></aside>
