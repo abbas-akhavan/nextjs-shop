c59adec55eb3dc46c793459554dfdfdf69b703bf
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-06 11:36:36 +0330

    convert o persian number the discounted percent

--- components/shared/DiscountPercent.tsx
+++ components/shared/DiscountPercent.tsx
@@ -1,8 +1,9 @@
+import { toPersianNumber } from '@/utils/useful-functions'
 import React from 'react'
 
 const DiscountPercent = ({ value }: { value: number }) => {
     return (
-        <span className='bg-red-600 text-white rounded-full px-2 py-[2px] ltr'>{value}%</span>
+        <span className='bg-red-600 text-white rounded-full px-2 py-[2px] ltr'>{toPersianNumber(value)}%</span>
     )
 }
 
