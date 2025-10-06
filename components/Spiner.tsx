8ea0f139e01b27bdb8879b6aad2026b1db7ce91d
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-06 22:29:24 +0330

    add className prop to Spiner

--- components/Spiner.tsx
+++ components/Spiner.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Spiner = () => {
+const Spiner = ({ className }: { className?: string }) => {
     return (
-        <div className="w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin"></div>
+        <div className={`w-4 h-4 border-2 border-white border-t-transparent rounded-full animate-spin ${className}`}></div>
     )
 }
 
