9dc3df5856cd16658d32ddc801ebad5de933eefd
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-07 17:13:08 +0330

    login button display bug fix

--- components/header/LoginOrRegisterButton.tsx
+++ components/header/LoginOrRegisterButton.tsx
@@ -29,11 +29,11 @@ const LoginOrRegisterButton = () => {
         <>
             {
                 user.userInfo?.isLoggedIn
-                    ? <Button variant="destructive" className='!size-9 hidden md:block md:!w-auto md:!h-9 ' onClick={() => signOut()}>
+                    ? <Button variant="destructive" className='!size-9 hidden md:inline-flex md:!w-auto md:!h-9 ' onClick={() => signOut()}>
                         <ArrowLeftStartOnRectangleIcon className='!size-6' />
                         <span className='hidden md:inline-block'>خروج</span>
                     </Button>
-                    : <Button asChild className='!size-9 hidden md:block md:!w-auto md:!h-9 border border-slate-600 bg-transparent hover:bg-gray-900'>
+                    : <Button asChild className='!size-9 hidden md:inline-flex md:!w-auto md:!h-9 border border-slate-600 bg-transparent hover:bg-gray-900'>
                         <Link href="/auth/login" >
                             <ArrowLeftEndOnRectangleIcon className='!size-6' />
                             <span className='hidden md:inline-block'>ورود | ثبت نام</span>
