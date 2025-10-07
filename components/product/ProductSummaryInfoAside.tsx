a75d7bdfc515ac40aa5aeb975efb7a0bc6624b2b
Abbas Akhavan <mr.abbas.akhavan@gmail.com>
2025-10-08 03:01:12 +0330

    show toast after add to cart

--- components/product/ProductSummaryInfoAside.tsx
+++ components/product/ProductSummaryInfoAside.tsx
@@ -8,6 +8,7 @@ import useAppStore from '@/store/useAppStore';
 import { useRouter } from 'next/navigation';
 import Spiner from '../Spiner';
 import CartButton from '../shared/CartButton';
+import toast from 'react-hot-toast';
 
 interface Props {
     product: Product;
@@ -18,9 +19,10 @@ const ProductSummaryInfoAside = ({ product }: Props) => {
     const user = useAppStore(state => state.user)
     const productInCart = useAppStore(state => state.cart.cartItems.find((cartItem) => cartItem.product.id === product.id))
     const router = useRouter();
-    function handleAddToCart() {
+    async function handleAddToCart() {
         if (!user) return router.push('/auth/login')
-        addToCart(product);
+        const result = await addToCart(product);
+        if (result) toast.success('کالا به سبد اضافه شد')
     }
     return (
         <div className='bg-slate-700 h-fit p-2 rounded-md sticky top-20 border border-slate-500 flex flex-col gap-3'>
@@ -30,7 +32,7 @@ const ProductSummaryInfoAside = ({ product }: Props) => {
                 <span>دیجی کالا</span>
             </div>
             <hr className='hidden lg:block border-slate-500' />
-            <div className='fixed bottom-0 left-0 w-full flex flex-row-reverse justify-between bg-slate-700 py-4 px-3 md:p-3 border-t border-slate-500 gap-3 lg:flex-col lg:static lg:bg-inherit lg:p-0'>
+            <div className='fixed bottom-0 left-0 w-full flex flex-row-reverse justify-between bg-slate-700 py-4 px-3 md:p-3 border-t border-slate-500 gap-3 lg:border-none lg:flex-col lg:static lg:bg-inherit lg:p-0'>
                 <div className='flex justify-end'>
                     <Price value={product.price} />
                 </div>
