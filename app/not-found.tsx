2cd4178b51e3ae1b040171e03bcc53abeb1b45e0
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-10 19:57:59 +0330

    change style of notFound page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,18 @@
+import { ChevronLeftIcon } from '@heroicons/react/24/solid'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const NotFound = () => {
   return (
-    <div>NotFound</div>
+    <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-3/5 flex flex-col gap-4 items-center font-semibold'>
+      <Image className='shadow-lg rounded-xl w-full max-w-[600]' src='/images/notFound.jpg' alt='notFound' width={300} height={300} />
+      <div className='text-lg'>صفحه مورد نظر پیدا نشد !</div>
+      <Link href='/' className='text-xs flex items-center gap-2 py-2 px-4 border border-slate-700 bg-transparent hover:bg-slate-950 rounded-full md:text-base'>
+        بازگشت به صفحه اصلی
+        <ChevronLeftIcon className='w-4 h-4' />
+      </Link>
+    </div>
   )
 }
 
