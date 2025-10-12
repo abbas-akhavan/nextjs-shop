3dca98e99647c496e34944adf8040127e76615e9
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-12 21:46:04 +0330

    create ProductsAsideFilters

---
+++ components/product/ProductsAsideFilters.tsx
@@ -0,0 +1,121 @@
+import { useSearchParams } from 'next/navigation';
+import React, { useState } from 'react'
+import CollabsibleContent from '../shared/CollabsibleContent';
+import CategoryFilterItem from './CategoryFilterItem';
+import { useQuery } from '@tanstack/react-query';
+import { Category } from '@/types/Category';
+import { fetchFromSupabaseWithAxios } from '@/utils/helpers';
+import { Button } from '../ui/button';
+import { Input } from '../ui/input';
+
+const ProductsAsideFilters = ({ handleFilters }: { handleFilters: (key: string, value: string) => void }) => {
+    const searchParams = useSearchParams();
+    const [floorPrice, setFloorPrice] = useState(() => {
+        const priceParam = searchParams.get('and');
+        if (priceParam?.includes('price')) {
+            const floorAndCeilPrice = priceParam.match(/\d+/g);
+            if (floorAndCeilPrice?.length === 2) {
+                return floorAndCeilPrice[0]
+            }
+            else {
+                return ''
+            }
+        } else {
+            return ''
+        }
+    })
+    const [ceilPrice, setCeilPrice] = useState(() => {
+        const priceParam = searchParams.get('and');
+        if (priceParam?.includes('price')) {
+            const floorAndCeilPrice = priceParam.match(/\d+/g);
+            if (floorAndCeilPrice?.length === 2) {
+                return floorAndCeilPrice[1]
+            }
+            else {
+                return ''
+            }
+        } else {
+            return ''
+        }
+    })
+
+    function handleChangeCheckBox(e: React.ChangeEvent<HTMLInputElement>) {
+        if (e.target.checked) {
+            handleFilters('category_id', `eq.${e.target.value}`)
+        } else {
+            handleFilters('category_id', '')
+        }
+
+    }
+    function handleApplyPrice() {
+        if (ceilPrice.trim() && floorPrice.trim()) {
+            handleFilters('and', `(price.gte.${floorPrice},price.lte.${ceilPrice})`)
+        } else if (ceilPrice.trim() || floorPrice.trim()) {
+            toast.error('لطفا هر دو مقدار را وارد کنید')
+        } else {
+            handleFilters('and', '')
+        }
+    }
+    function handlePriceChange(key: string, value: string) {
+        if (Number(value) || value === '') {
+            if (key === 'floor') {
+                setFloorPrice(value)
+            } else if (key === 'ceil') {
+                setCeilPrice(value)
+            }
+        }
+    }
+
+    const {
+        error: categoryError,
+        isLoading: categoryIsLoading,
+        data: categories
+    } = useQuery<{ data: Category[], totalRows: number, currentRow: number }>({
+        queryKey: ['categories'],
+        queryFn: () => fetchFromSupabaseWithAxios('categories', {
+            select: '*'
+        })
+    })
+
+    return (
+        <div className='border rounded-md border-slate-700 bg-slate-800 sticky top-20 p-4'>
+            <div className='text-lg font-semibold'>فیلترها</div>
+            <CollabsibleContent title='دسته بندی'>
+                <CategoryFilterItem
+                    key='all-category'
+                    title='همه کالاها'
+                    active={!searchParams.get('category_id')}
+                    onClick={() => handleFilters('category_id', '')}
+                />
+                {
+                    categories?.data.map(category => (
+                        <CategoryFilterItem
+                            key={category.id}
+                            title={category.name}
+                            active={searchParams.get('category_id') === `eq.${category.id}`}
+                            onClick={() => handleFilters('category_id', `eq.${category.id}`)}
+                        />
+                    ))
+                }
+            </CollabsibleContent>
+            <CollabsibleContent title='محدوده قیمت'>
+                <div className='flex flex-col items-center gap-3'>
+                    <div className='flex gap-2 items-center w-full'>
+                        <span className='w-4 shrink-0'>از</span>
+                        <Input className='border-slate-600 ltr' value={floorPrice} onChange={(e) => handlePriceChange('floor', e.target.value)} />
+                        <span>تومان</span>
+                    </div>
+                    <div className='flex gap-2 items-center w-full'>
+                        <span className='w-4 shrink-0'>تا</span>
+                        <Input className='border-slate-600 ltr' value={ceilPrice} onChange={(e) => handlePriceChange('ceil', e.target.value)} />
+                        <span>تومان</span>
+                    </div>
+                    <Button className='border border-slate-700 bg-transparent hover:bg-slate-950' onClick={handleApplyPrice}>اعمال محدوده قیت</Button>
+                </div>
+
+            </CollabsibleContent>
+        </div>
+    )
+}
+
+export default ProductsAsideFilters
--- components/product/ProductsList.tsx
+++ components/product/ProductsList.tsx
@@ -1,20 +1,15 @@
 'use client'
 import { Product } from '@/types/Product';
 import { fetchFromSupabaseWithAxios } from '@/utils/helpers';
-import { BarsArrowDownIcon, CheckIcon } from '@heroicons/react/24/solid';
-import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
+import { BarsArrowDownIcon } from '@heroicons/react/24/solid';
+import { useInfiniteQuery } from '@tanstack/react-query';
 import { useRouter, useSearchParams } from 'next/navigation';
-import React, { useEffect, useRef, useState } from 'react'
-import CollabsibleContent from '../shared/CollabsibleContent';
+import React, { useEffect, useRef } from 'react'
 import SortItem from './SortItem';
 import ProductItemCard from './ProductItemCard';
-import { Category } from '@/types/Category';
-import CategoryFilterItem from './CategoryFilterItem';
-import { Input } from '../ui/input';
-import { Button } from '../ui/button';
-import toast from 'react-hot-toast';
 import ProductItemCardSkelrton from '../skeletons/ProductItemCardSkelrton';
 import Spiner from '../Spiner';
+import ProductsAsideFilters from './ProductsAsideFilters';
 
 const ProductsList = () => {
     // http://localhost:3005/products?category_id=eq.1&order=price.desc
@@ -23,34 +18,6 @@ const ProductsList = () => {
     const filters = Object.fromEntries(searchParams.entries())
     const productPerPage = 10;
     const loadMoreRef = useRef<HTMLDivElement | null>(null);
-    const [floorPrice, setFloorPrice] = useState(() => {
-        const priceParam = searchParams.get('and');
-        if (priceParam?.includes('price')) {
-            const floorAndCeilPrice = priceParam.match(/\d+/g);
-            if (floorAndCeilPrice?.length === 2) {
-                return floorAndCeilPrice[0]
-            }
-            else {
-                return ''
-            }
-        } else {
-            return ''
-        }
-    })
-    const [ceilPrice, setCeilPrice] = useState(() => {
-        const priceParam = searchParams.get('and');
-        if (priceParam?.includes('price')) {
-            const floorAndCeilPrice = priceParam.match(/\d+/g);
-            if (floorAndCeilPrice?.length === 2) {
-                return floorAndCeilPrice[1]
-            }
-            else {
-                return ''
-            }
-        } else {
-            return ''
-        }
-    })
 
     function handleFilters(key: string, value: string) {
         const params = new URLSearchParams(searchParams.toString());
@@ -62,33 +29,7 @@ const ProductsList = () => {
 
         router.push(`?${params.toString()}`)
     }
-    function handleChangeCheckBox(e: React.ChangeEvent<HTMLInputElement>) {
-        if (e.target.checked) {
-            handleFilters('category_id', `eq.${e.target.value}`)
-        } else {
-            handleFilters('category_id', '')
-        }
 
-    }
-    function handleApplyPrice() {
-        if (ceilPrice.trim() && floorPrice.trim()) {
-            handleFilters('and', `(price.gte.${floorPrice},price.lte.${ceilPrice})`)
-        } else if (ceilPrice.trim() || floorPrice.trim()) {
-            toast.error('لطفا هر دو مقدار را وارد کنید')
-        } else {
-            handleFilters('and', '')
-        }
-    }
-
-    function handlePriceChange(key: string, value: string) {
-        if (Number(value) || value === '') {
-            if (key === 'floor') {
-                setFloorPrice(value)
-            } else if (key === 'ceil') {
-                setCeilPrice(value)
-            }
-        }
-    }
     const {
         data: products,
         isFetching,
@@ -116,16 +57,6 @@ const ProductsList = () => {
         initialPageParam: 0,
         refetchOnWindowFocus: false
     });
-    const {
-        error: categoryError,
-        isLoading: categoryIsLoading,
-        data: categories
-    } = useQuery<{ data: Category[], totalRows: number, currentRow: number }>({
-        queryKey: ['categories'],
-        queryFn: () => fetchFromSupabaseWithAxios('categories', {
-            select: '*'
-        })
-    })
 
     useEffect(() => {
         if (!loadMoreRef.current) return
@@ -148,43 +79,7 @@ const ProductsList = () => {
     return (
         <div className='container grid grid-cols-[300px_1fr] gap-3 my-5'>
             <aside>
-                <div className='border rounded-md border-slate-700 bg-slate-800 sticky top-20 p-4'>
-                    <div className='text-lg font-semibold'>فیلترها</div>
-                    <CollabsibleContent title='دسته بندی'>
-                        <CategoryFilterItem
-                            key='all-category'
-                            title='همه کالاها'
-                            active={!searchParams.get('category_id')}
-                            onClick={() => handleFilters('category_id', '')}
-                        />
-                        {
-                            categories?.data.map(category => (
-                                <CategoryFilterItem
-                                    key={category.id}
-                                    title={category.name}
-                                    active={searchParams.get('category_id') === `eq.${category.id}`}
-                                    onClick={() => handleFilters('category_id', `eq.${category.id}`)}
-                                />
-                            ))
-                        }
-                    </CollabsibleContent>
-                    <CollabsibleContent title='محدوده قیمت'>
-                        <div className='flex flex-col items-center gap-3'>
-                            <div className='flex gap-2 items-center w-full'>
-                                <span className='w-4 shrink-0'>از</span>
-                                <Input className='border-slate-600 ltr' value={floorPrice} onChange={(e) => handlePriceChange('floor', e.target.value)} />
-                                <span>تومان</span>
-                            </div>
-                            <div className='flex gap-2 items-center w-full'>
-                                <span className='w-4 shrink-0'>تا</span>
-                                <Input className='border-slate-600 ltr' value={ceilPrice} onChange={(e) => handlePriceChange('ceil', e.target.value)} />
-                                <span>تومان</span>
-                            </div>
-                            <Button className='border border-slate-700 bg-transparent hover:bg-slate-950' onClick={handleApplyPrice}>اعمال محدوده قیت</Button>
-                        </div>
-
-                    </CollabsibleContent>
-                </div>
+                <ProductsAsideFilters handleFilters={handleFilters} />
             </aside>
             <section>
                 <div className='flex gap-2 py-2 border-b border-slate-700 text-sm items-center'>
