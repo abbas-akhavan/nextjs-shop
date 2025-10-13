8238f3001060f434105827aa6c35f04679320f18
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-13 21:27:11 +0330

    ProductsMobileFilter title

--- components/product/ProductsMobileFilter.tsx
+++ components/product/ProductsMobileFilter.tsx
@@ -85,11 +85,11 @@ const ProductsMobileFilter = ({ handleFilters }: { handleFilters: (key: string, 
             fill={true}
             open={open}
             onOpenChange={setOpen}
-            title='فیلتر'
+            title='فیلترها'
             triggerContent={
                 <span className='flex gap-2 items-center text-xs border border-slate-700 rounded-full py-2 px-3'>
                     <FunnelIcon className='w-5 h-5' />
-                    فیلترها
+                    فیلتر
                 </span>
             }
         >
