64416c72cf9287bb6825f9d823d05bbb74a6ba41
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-13 18:56:05 +0330

    dfine ProductsMobileSort how to show

--- components/ui/MobileBottomModal.tsx
+++ components/ui/MobileBottomModal.tsx
@@ -14,9 +14,7 @@ const MobileBottomModal = ({ title, open, onOpenChange, children, fill, triggerC
         <Dialog open={open} onOpenChange={onOpenChange}>
             <DialogTrigger>{triggerContent}</DialogTrigger>
             <DialogContent
-                className={
-                    `${fill ? 'h-screen' : ''} bg-slate-800 border-0 !duration-300 [&>button]:left-4 [&>button]:right-auto bottom-0 top-auto translate-y-0 left-0 translate-x-0 w-screen max-w-full data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-1 data-[state=open]:fade-in-1 data-[state=closed]:zoom-out-100 data-[state=open]:zoom-in-100 data-[state=closed]:slide-out-to-left-0 data-[state=open]:slide-in-from-left-0 data-[state=open]:slide-in-from-bottom-full data-[state=closed]:slide-out-to-bottom-full`
-                }
+                className={` bg-slate-800 border-0 !duration-300 [&>button]:left-4 [&>button]:right-auto bottom-0 top-auto translate-y-0 left-0 translate-x-0 w-screen max-w-full data-[state=open]:animate-in data-[state=closed]:!animate-out data-[state=closed]:!fade-out-1 data-[state=open]:!fade-in-1 data-[state=closed]:!zoom-out-100 data-[state=open]:!zoom-in-100 data-[state=closed]:!slide-out-to-left-0 data-[state=open]:!slide-in-from-left-0 data-[state=open]:!slide-in-from-bottom-full data-[state=closed]:!slide-out-to-bottom-full ${fill ? 'h-screen' : ''}`}
             >
                 {
                     title && <DialogTitle>{title}</DialogTitle>
