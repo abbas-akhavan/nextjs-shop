d0bb9428ddad7cfc80db1b36f696e0913604efa3
Abbas Akhavan <abbas.akhavan1@yahoo.com>
2025-10-14 01:54:10 +0330

    fix loading component position bug

--- components/LoadingComponent.tsx
+++ components/LoadingComponent.tsx
@@ -3,10 +3,12 @@ import React from 'react'
 
 const LoadingComponent = ({ className }: { className?: string }) => {
     return (
-        <span className={`fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 flex size-14 animate-bounce ${className}`}>
-            <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-gradient-to-br from-slate-500 to-gray-300 blur-[1px] opacity-75"></span>
-            <span className="relative inline-flex h-full w-full rounded-full bg-gradient-to-br from-slate-500 to-gray-300 animate-spin"></span>
-        </span>
+        <div className='fixed top-1/2 left-1/2 -translate-y-1/2 -translate-x-1/2 '>
+            <span className={`flex size-14 animate-bounce ${className}`}>
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-gradient-to-br from-slate-500 to-gray-300 blur-[1px] opacity-75"></span>
+                <span className="relative inline-flex h-full w-full rounded-full bg-gradient-to-br from-slate-500 to-gray-300 animate-spin"></span>
+            </span>
+        </div>
     )
 }
 
